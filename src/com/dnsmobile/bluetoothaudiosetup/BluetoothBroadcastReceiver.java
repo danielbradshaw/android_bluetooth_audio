bfc3f31afb229567b52fb4acedecb87c00bcfb76
Daniel Bradshaw <daniel.a.bradshaw@gmail.com>
2013-03-08 00:43:52 -0800

    Add functionality to restore the original volume after disconnecting.

--- src/com/dnsmobile/bluetoothaudiosetup/BluetoothBroadcastReceiver.java
+++ src/com/dnsmobile/bluetoothaudiosetup/BluetoothBroadcastReceiver.java
@@ -11,6 +11,7 @@ import android.util.Log;
 public class BluetoothBroadcastReceiver extends BroadcastReceiver 
 {
 	private static final String TAG = "BluetoothBroadcastReceiver";
+	public static final String SHARED_PREFS_ORIGINAL_VOL = "com.dnsmobile.bluetoothaudiosetup.ORIGINAL_VOL";
 	private static final int AUDIO_STREAM_BLUETOOTH = 6;
 	
 	@Override
@@ -29,11 +30,11 @@ public class BluetoothBroadcastReceiver extends BroadcastReceiver
 			switch (intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, 0)) {
 				case BluetoothAdapter.STATE_CONNECTED:
 					Log.i(TAG, "Bluetooth has connected!");
-					bluetoothConnected(context);
+					bluetoothConnected(context, sharedPrefs);
 					break;
 				case BluetoothAdapter.STATE_DISCONNECTED:
 					Log.i(TAG, "Bluetooth disconnected!");
-					bluetoothDisconnected(context);
+					bluetoothDisconnected(context, sharedPrefs);
 					break;
 				default:
 					break;
@@ -41,20 +42,25 @@ public class BluetoothBroadcastReceiver extends BroadcastReceiver
 		}
 	}
 	
-	private void bluetoothConnected(Context context) {
+	private void bluetoothConnected(Context context, SharedPreferences sharedPrefs) {
 		
 		// create the service to keep the process alive
 		context.startService(new Intent(context, WakeLockService.class));
 		
-		// set bluetooth audio to full volume
+		// set bluetooth audio to full volume, saving the original
 		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		int originalVolume = audioManager.getStreamVolume(AUDIO_STREAM_BLUETOOTH);
+		sharedPrefs.edit().putInt(SHARED_PREFS_ORIGINAL_VOL, originalVolume).commit();
 		int maxVolume = audioManager.getStreamMaxVolume(AUDIO_STREAM_BLUETOOTH);
 		audioManager.setStreamVolume(AUDIO_STREAM_BLUETOOTH, maxVolume, AudioManager.FLAG_SHOW_UI);
 	}
 	
-	private void bluetoothDisconnected(Context context) {
+	private void bluetoothDisconnected(Context context, SharedPreferences sharedPrefs) {
 		
-		// potentially reset audio level
+		// reset audio level to the original volume
+		AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		int originalVolume = sharedPrefs.getInt(SHARED_PREFS_ORIGINAL_VOL, 0);
+		audioManager.setStreamVolume(AUDIO_STREAM_BLUETOOTH, originalVolume, AudioManager.FLAG_SHOW_UI);
 		
 		// stop the service which will remove the notification and release the wake lock
 		context.stopService(new Intent(context, WakeLockService.class));
